09e1df171611610a862d307fe110b0476c192a65
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-05-24 15:27:30 +0530

    Small Color Correction

--- src/pages/ReactJS/Tic-Tac-Toe-App/Square.jsx
+++ src/pages/ReactJS/Tic-Tac-Toe-App/Square.jsx
@@ -5,7 +5,7 @@ class Square extends Component {
         const { value, onClick } = this.props;
 
         return (
-        <button className="group h-[100px] w-[100px] dark:bg-gray-400 bg-white border-2 dark:border-pink-600 border-pink-600 rounded-md" onClick={onClick}>
+        <button className="group h-[100px] w-[100px] dark:bg-[rgb(27,27,39)] bg-white border-2 dark:border-pink-600 border-pink-600 rounded-md" onClick={onClick}>
             {value === 'X' ?
                 <h1 className='font-bold text-5xl text-blue-900'>{value}</h1> :
                 <h1 className='font-bold text-5xl text-green-800'>{value}</h1> 
